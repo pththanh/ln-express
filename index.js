2895d0abae904d819531b098bd530b3668509573
Hoai Thanh Pham Truong <hoai-thanh.pham-truong@ekino.com>
2024-01-20 15:35:45 +0700

    chore(): create login route

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/login', (req, res) => {
+  res.send('Login Page!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
